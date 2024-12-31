ccd419edd2a94a298b36482d649e4ee0b61a2e1b
Himanshu <prajapathimanshu51@gmail.com>
2024-12-31 13:30:53 +0530

    updated logo versioning

--- resources/views/_partials/navbar-dark.blade.php
+++ resources/views/_partials/navbar-dark.blade.php
@@ -3,7 +3,7 @@
     <div class="flex justify-between px-4 md:px-10 container  mx-auto">
         <div class="logo flex justify-center items-center">
             <a href="{{route('frontend.home')}}">
-                <img class="h-16 text-white invert-0" src="{{ asset('assets/images/logo-light.svg') }}"  alt="Logo">
+                <img class="h-16 text-white invert-0" src="{{ asset('assets/images/logo-light.svg?v=2') }}"  alt="Logo">
             </a>
         </div>
         <div class="sm:hidden flex items-center">
