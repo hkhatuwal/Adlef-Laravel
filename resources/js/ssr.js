471e015c53787e0304583ad10b804802d730d024
Himanshu <prjapathimanshu51@gmail.com>
2025-03-12 15:36:39 +0530

    Fix Vite Css include issue in build

--- resources/js/ssr.js
+++ resources/js/ssr.js
@@ -1,7 +1,7 @@
 import { createServer } from "http";
 import { createSSRApp } from "vue";
 import { renderToString } from "@vue/server-renderer";
-import { renderSpladeApp, SpladePlugin, startServer } from "@protonemedia/laravel-splade";
+
 
 startServer(createServer, renderToString, (props) => {
     return createSSRApp({
