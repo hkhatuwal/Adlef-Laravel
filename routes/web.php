d07a585d85bf2a725a97b03b8ab71c6deca4c467
Himanshu <prjapathimanshu51@gmail.com>
2025-05-22 21:51:56 +0530

    upate the run time to hourly

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,6 @@ Route::get('install', function () {
 });
 Route::get('/run-scheduler', function () {
     Artisan::call('schedule:run');
-    Artisan::call('queue:work');
     return 'Scheduler executed at ' . now();
 });
 Route::get('storage', function () {
