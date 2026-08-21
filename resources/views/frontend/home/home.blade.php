5510c788bedb40550d4c7895954d87ed0ca2d71f
Himanshu <prajapathimanshu51@gmail.com>
2026-08-21 23:09:32 +0800

    home text

--- resources/views/frontend/home/home.blade.php
+++ resources/views/frontend/home/home.blade.php
@@ -7,9 +7,10 @@
     <section class="container mx-auto hero flex flex-col-reverse md:flex-row items-center bg-white p-3 md:p-10">
         <div class="hero-content flex-1 flex flex-col items-center md:items-start gap-4">
             <h3 class="text-3xl mt-4 md:text-[3.5rem] font-visuletProLight leading-tight text-center md:text-start">
-                Corporate <br>
-                <mark class="font-semibold bg-accent">Payment <br>
-                    Services for
+
+                Financial <br>
+                <mark class="font-semibold bg-accent">Infrastructure <br>
+                     for
                 </mark>
                 <br>global <br> businesses.
             </h3>
